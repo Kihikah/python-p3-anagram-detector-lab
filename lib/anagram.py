552f154ad5c04851c9eb65731608202ac57b5178
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-05-08 11:29:12 +0300

    Anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_word  = sorted(self.word)
+        return [
+            candidate for candidate in word_list
+            if sorted(candidate.lower()) == sorted_word and candidate.lower() != self.word
+        ]
+        pass
+    
